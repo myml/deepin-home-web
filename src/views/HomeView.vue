98a5c88e97b63eb321fc4d516a6caa58369b4fce
吴没有 <980952617@qq.com>
2024-11-07 01:56:43 +0800

    1.引入elementUI库 2.自定义轮播图组件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
   <main>
+    <NCarousel :value-list="imageList" style="margin-top: 20px">
+      <template #default="val">
+        <div style="width: 100%; height: 100%; overflow: hidden; border-radius: 20px;">
+          <img :src="val.value" style="width: 100%; height: 100%; object-fit: cover;" alt=""/>
+        </div>
+      </template>
+    </NCarousel>
     <h1>{{ t('home.hello') }}</h1>
   </main>
 </template>
 
 <script setup>
 import { useI18n } from 'vue-i18n'
+import NCarousel from '@/components/nCarousel.vue'
 
+const imageList = [
+  'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+  'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+  'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+]
 const { t } = useI18n()
 </script>

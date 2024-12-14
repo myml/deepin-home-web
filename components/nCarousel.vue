30624997067d986d4160e4c6c2071d91ad8a363b
Patrick Tien <ivetien@outlook.com>
2024-12-14 23:57:57 +0800

    将elementui的轮播图组件改成swiperjs

--- components/nCarousel.vue
+++ components/nCarousel.vue
@@ -1,63 +1,63 @@
 <template>
-  <el-carousel
-    indicator-position="none"
-    arrow="never"
-    :interval="interval"
-    :height="height"
-    type="card"
-    :card-scale="1"
-    @change="change">
-    <el-carousel-item
+  <swiper
+    :modules="[EffectCards, Autoplay]"
+    :effect="'cards'"
+    :grab-cursor="true"
+    :loop="true"
+    :autoplay="{
+      delay: interval,
+      disableOnInteraction: false
+    }"
+    class="w-full"
+    :style="{ height }"
+    @slide-change="handleSlideChange">
+    <swiper-slide
       v-for="(item, index) in filterExpired()"
       :key="index"
-      :class="`class${index}`">
-      <div class="w-full h-full overflow-hidden rounded-2xl">
-        <div class="w-full h-full relative font-semibold">
-          <img
-            :src="item.image"
-            alt="carousel"
-            class="fixed top-0 left-0 right-0 h-full object-fill rounded-2xl"
-            @click="openUrl(item.image_link, index)" />
-          <span
-            v-if="item.title"
-            class="absolute left-[53px] top-[183px] text-3xl text-[--website-layer-card-background]"
-            >{{ item.title }}</span
-          >
-          <span
-            v-if="item.content"
-            class="absolute left-[53px] top-[237px] text-xl text-[--website-layer-card-background] max-w-[370px]"
-            style="min-width: 200px">
-            {{ item.content }}
-          </span>
+      class="rounded-2xl overflow-hidden">
+      <div class="w-full h-full relative font-semibold">
+        <img
+          :src="item.image"
+          alt="carousel"
+          class="w-full h-full object-fill rounded-2xl"
+          @click="openUrl(item.image_link, index)" />
+        <span
+          v-if="item.title"
+          class="absolute left-[53px] top-[183px] text-3xl text-[--website-layer-card-background]">
+          {{ item.title }}
+        </span>
+        <span
+          v-if="item.content"
+          class="absolute left-[53px] top-[237px] text-xl text-[--website-layer-card-background] max-w-[370px]">
+          {{ item.content }}
+        </span>
+        <div
+          v-if="item.links.length"
+          class="absolute left-[53px] bottom-[33px] flex gap-2">
           <div
-            v-if="item.links.length"
-            class="absolute left-[53px] bottom-[33px] flex gap-2">
-            <div
-              v-for="(link, linkIndex) in item.links"
-              :key="linkIndex"
-              class="px-[22px] py-3 text-lg text-[--website-font-primary] border border-[--website-tag-border] bg-[--website-layer-card-background] rounded-[39px]"
-              @click="openUrl(link.url)">
-              {{ link.title }}
-            </div>
+            v-for="(link, linkIndex) in item.links"
+            :key="linkIndex"
+            class="px-[22px] py-3 text-lg text-[--website-font-primary] border border-[--website-tag-border] bg-[--website-layer-card-background] rounded-[39px]"
+            @click="openUrl(link.url)">
+            {{ link.title }}
           </div>
         </div>
       </div>
-    </el-carousel-item>
-  </el-carousel>
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script setup lang="ts">
-import { ref, nextTick, onMounted, onBeforeUnmount } from 'vue'
+import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { EffectCards, Autoplay } from 'swiper/modules'
 import type { CarouselCard } from '@/api/model'
 import { dayjs } from 'element-plus'
 
-/**
- * 轮播图组件
- * @props:
- *  - valueList: 数据列表 必填 最少3个
- *  - interval: 轮播间隔时间 默认5000ms
- *  - height: 轮播高度 默认400px
- */
+// 引入 Swiper 样式
+import 'swiper/css'
+import 'swiper/css/effect-cards'
+
 const props = defineProps({
   valueList: {
     type: Array<CarouselCard>,
@@ -74,61 +74,18 @@ const props = defineProps({
   }
 })
 
-const handleResize = () => {
-  adjustCarouselItems()
-}
-
-onMounted(() => {
-  window.addEventListener('resize', handleResize)
-  adjustCarouselItems()
-})
-
-onBeforeUnmount(() => {
-  window.removeEventListener('resize', handleResize)
-})
-
 const current = ref(0)
 
-function change(_current: number) {
-  current.value = _current
-  adjustCarouselItems()
-}
-
-function adjustCarouselItems() {
-  nextTick(() => {
-    const length = props.valueList.length
-    let lefts: HTMLDivElement | null = null
-    let rights: HTMLDivElement | null = null
-    if (current.value === 0) {
-      lefts = document.getElementsByClassName(
-        `class${length - 1}`
-      )[0] as HTMLDivElement
-      rights = document.getElementsByClassName(`class${1}`)[0] as HTMLDivElement
-    } else {
-      lefts = document.getElementsByClassName(
-        `class${current.value - 1}`
-      )[0] as HTMLDivElement
-      rights = document.getElementsByClassName(
-        `class${current.value === length - 1 ? 0 : current.value + 1}`
-      )[0] as HTMLDivElement
-    }
-    if (lefts) {
-      lefts.style.transform = `translateX(-${55}%)`
-    }
-    if (rights) {
-      rights.style.transform = `translateX(${155}%)`
-    }
-  })
+const handleSlideChange = (swiper: SwiperType) => { // 使用具体的 Swiper 类型替代 any
+  current.value = swiper.activeIndex
 }
 
 const openUrl = (url: string, index: number = -1) => {
-  // 如果点击的图片在中间，才打开链接
   if (index !== current.value) return
   if (!url) return
   window.open(url)
 }
 
-// 从valueList里过滤掉过期的轮播图
 const filterExpired = () => {
   return props.valueList.filter(item => {
     const now = dayjs()
@@ -136,3 +93,15 @@ const filterExpired = () => {
   })
 }
 </script>
+
+<style lang="scss" scoped>
+.swiper {
+  width: min(1024px, 90vw);
+  margin: 0 auto;
+}
+
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+}
+</style>
